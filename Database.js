3a2192504b45e2017108a85806d8d3b0aecd1454
MAMAR HABTAMU <mamarhabtamu570@Gmail.com>
2022-03-26 11:56:04 -0700

    hi there

--- Database.js
+++ Database.js
@@ -2,9 +2,9 @@ var express = require('express');
 const router=express.Router()
 const mysql=require('mysql');
 var Connection=mysql.createConnection({
-    host:"localhost", 
+    host:"mysql-73517-db.mysql-73517:19073 ", 
     user:"root",
-    password:"",
+    password:"0949050454",
     database:"ict",
 })
 Connection.connect(function(err) {
