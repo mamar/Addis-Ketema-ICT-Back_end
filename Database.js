897f6a7fe65208759129ab0bb659e01c5f2d382d
MAMAR HABTAMU <mamarhabtamu570@Gmail.com>
2022-04-07 09:34:14 -0700

    hi there

--- Database.js
+++ Database.js
@@ -1,14 +1,14 @@
 var express = require('express');
 const router=express.Router()
 const mysql=require('mysql');
-var Connection=mysql.createConnection({
+var Connection=mysql.createPool({
     host:"remotemysql.com", 
     port:"3306",
     user:"V8C3p0QyK2",
     password:"1DP4vbkGZC",
     database:"V8C3p0QyK2"
 })
-Connection.connect(function(err) {
+Connection.getConnection(function(err) {
     
     if (err) throw err
 
